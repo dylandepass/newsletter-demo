d3e67e8ee3359e28169e8f646f0fc2e6eeb3b74d
dyland <dylandepass@gmail.com>
2022-05-20 12:21:11 -0400

    fix: remove test comment

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -11,7 +11,6 @@
  */
 
 export default async function decorate(block) {
-  // Try and trigger codebus update?
   block.innerHTML = /*html*/`
     <table width="100%" style="border: 0px;border-collapse: collapse;border-spacing: 0px;"> 
       <tbody>
